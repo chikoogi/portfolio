b07472555a61c8f856b4db6934349607a062123a
jihun_kim <jihun_kim8@tmax.co.kr>
2024-09-11 18:29:03 +0900

    [New feature] Link add

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -34,7 +34,7 @@ export default function PortfolioHome() {
         className={`fixed top-0 left-0 w-full z-10 p-4 font-[Rajdhani] flex justify-center items-center`}
       >
         <ul
-          className={`flex justify-center space-x-4 text-primary-3 gap-10 p-5 rounded-xl ${scrollTop ? "bg-transparent" : "bg-primary-1"}`}
+          className={`flex justify-center space-x-4 text-primary-3 gap-10 px-5 py-7 rounded-xl ${scrollTop ? "bg-transparent" : "bg-primary-1 opacity-80"}`}
         >
           <li
             className={`border-t-[1px] border-primary-4 pt-4 hover:border-white hover:text-white`}
