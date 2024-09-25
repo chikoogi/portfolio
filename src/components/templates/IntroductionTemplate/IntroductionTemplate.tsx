e976957d64b6b85990c540ec84f8f98f643cfe29
jihun_kim <jihun_kim8@tmax.co.kr>
2024-09-25 09:42:22 +0900

    [New feature] - intro 문구 수정

--- src/components/templates/IntroductionTemplate/IntroductionTemplate.tsx
+++ src/components/templates/IntroductionTemplate/IntroductionTemplate.tsx
@@ -8,11 +8,11 @@ export default function IntroductionTemplate() {
         <div className={"text-center text-6xl mb-10"}>Portfolio</div>
         <div className={"text-center"}>
           <p className="text-sm leading-8 text-gray-300">
-            안녕하세요! 프론트엔드 개발자로서 사용자 경험을 향상시키는 데 중점을 두고 웹
-            애플리케이션을 개발하고 있습니다.
+            프론트엔드 개발자로서 사용자 경험을 향상시키는 데 중점을 두고 웹 애플리케이션을 개발하고
+            있습니다.
             <br />
-            React를 주요 기술 스택으로 삼아, 최신 웹 트렌드와 디자인 패턴에 대한 깊은 이해를
-            바탕으로 혁신적인 솔루션을 제공하고 있습니다.
+            React를 주요 기술 스택으로 삼아, 효율적이고 직관적인 UI를 만드는 데 집중하며, 사용자의
+            경험을 최우선으로 생각합니다.
             <br />
             코드 품질과 지속적인 학습을 중요시하며, 새로운 기술과 도구를 활용하여 끊임없이 성장하고
             있습니다.
